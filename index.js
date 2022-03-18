069cf60d21b4287eeed752f663047fdd076b9f5b
Justin Ormont <justinormont@users.noreply.github.com>
2022-03-18 16:09:47 -0700

    Filter packets

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 
 const startScanning = (async () => {
     const filters = [];
-    
+
     const filterName = ''; //document.querySelector('#name').value;
     if (filterName) {
-        filters.push({name: filterName});
+        filters.push({ name: filterName });
     }
-    
+
     const filterNamePrefix = ''; //document.querySelector('#namePrefix').value;
     if (filterNamePrefix) {
-        filters.push({namePrefix: filterNamePrefix});
+        filters.push({ namePrefix: filterNamePrefix });
     }
-    
+
     const options = {};
 
     const allAdvertisements = true; //document.querySelector('#allAdvertisements').checked;
@@ -22,41 +22,47 @@ const startScanning = (async () => {
     } else {
         options.filters = filters;
     }
-    
+
     try {
         log('Requesting Bluetooth Scan with options: ' + JSON.stringify(options));
         const scan = await navigator.bluetooth.requestLEScan(options);
-    
+
         log('Scan started with:');
         log(' acceptAllAdvertisements: ' + scan.acceptAllAdvertisements);
         log(' active: ' + scan.active);
         log(' keepRepeatedDevices: ' + scan.keepRepeatedDevices);
         log(' filters: ' + JSON.stringify(scan.filters));
-    
+
         navigator.bluetooth.addEventListener('advertisementreceived', event => {
-        log('Advertisement received.');
-        log('  Device Name: ' + event.device.name);
-        log('  Device ID: ' + event.device.id);
-        log('  RSSI: ' + event.rssi);
-        log('  TX Power: ' + event.txPower);
-        log('  UUIDs: ' + event.uuids);
-        event.manufacturerData.forEach((valueDataView, key) => {
-            logDataView('Manufacturer', key, valueDataView);
+            const manufacturerData = event.manufacturerData.map((valueDataView, key) => logDataView('Manufacturer', key, valueDataView));
+            const serviceData = event.serviceData.map((valueDataView, key) => logDataView('Service', key, valueDataView));
+            
+            if (filter(serviceData)) {
+                log('\n\nAdvertisement received.');
+                log('  Device Name: ' + event.device.name);
+                log('  Device ID: ' + event.device.id);
+                log('  RSSI: ' + event.rssi);
+                log('  TX Power: ' + event.txPower);
+                log('  UUIDs: ' + event.uuids);
+                log(manufacturerData);
+                log(serviceData);
+            }
         });
-        event.serviceData.forEach((valueDataView, key) => {
-            logDataView('Service', key, valueDataView);
-        });
-        });
-    
+
         setTimeout(stopScan, 10000);
+
         function stopScan() {
-        log('Stopping scan...');
-        scan.stop();
-        log('Stopped.  scan.active = ' + scan.active);
+            log('Stopping scan...');
+            scan.stop();
+            log('Stopped.  scan.active = ' + scan.active);
         }
-    } catch(error)  {
+    } catch (error) {
         log('Argh! ' + error);
     }
+});
+
+const filter = ((str) => {
+    return str.match(/ 64 [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] [0-9a-f][0-9a-f] 00 00 00 $/);
 });
 
 const log = ((str) => {
@@ -66,3 +72,20 @@ const log = ((str) => {
 document.getElementById('goButton').addEventListener('click', (() => {
     startScanning();
 }));
+
+const logDataView = (labelOfDataSource, key, valueDataView) => {
+    const hexString = [...new Uint8Array(valueDataView.buffer)].map(b => {
+        return b.toString(16).padStart(2, '0');
+    }).join(' ');
+    
+    const textDecoder = new TextDecoder('ascii');
+    const asciiString = textDecoder.decode(valueDataView.buffer);
+    
+    const output = [
+        `\tlabel=${labelOfDataSource} Data: key=${key}`,
+        `\n\t\t(Hex) ${hexString}`,
+        `\n\t\t(ASCII) ${asciiString}`
+    ].join('');
+
+    return output;
+};
